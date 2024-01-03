355c87e68e18720d7185b5fc6dcd7d5ff446ab0b
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-01-04 00:56:40 +0200

    bed time

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,18 +1,15 @@
-import { Get, Post, Body, Controller } from '@nestjs/common'
+import { Post, Body, Controller } from '@nestjs/common'
 
 import { AppService } from './app.service'
+
+import { SignInDto } from '../../dtos'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
-  }
-
   @Post('/sign-in')
-  signIn(@Body('email') email: string, @Body('password') password: string) {
+  signIn(@Body() { email, password }: SignInDto) {
     return this.appService.signIn({ email, password })
   }
 }
--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@nestjs/common'
 
+import { SignInDto } from '../../dtos'
+
 async function delay(ms: number): Promise<void> {
   await new Promise(resolve => setTimeout(resolve, ms))
 }
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
-  }
-
-  async signIn({ email, password }) {
-    // return {
-    //   id: 'partsutsa-uuid-ne-aidi-ti',
-    //   email,
-    //   name: 'Dementsio Kolev',
-    // };
-
+  async signIn({ email, password }: SignInDto) {
     return delay(1500).then(() => {
       return {
         id: 'partsutsa-uuid-ne-aidi-ti',
